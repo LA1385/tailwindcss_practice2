22fcf4d6d0cfd919cbe2e03a0c4fd2ae05d13a3c
LA1385 <ahmadakanniluqman@gmail.com>
2025-11-05 22:53:18 +0100

    pricing,promo and footer section

--- scripts.js
+++ scripts.js
@@ -1,11 +1,5 @@
-var toggleBtn = document.querySelector("#toggle");
-var navBar = document.querySelector("#navBar");
-
-toggleBtn.addEventListener("click", () => {
-    if (navBar.classList.contains["hidden"]) {
-        navBar.classList.remove("hidden");
-    }
-    else {
-        navBar.classList.add("hidden");
-    }
-})
+// Copyright Section
+var Year = document.getElementById("year");
+var currentDate = new Date();
+var currentYear = currentDate.getFullYear();
+Year.innerHTML = currentYear;
